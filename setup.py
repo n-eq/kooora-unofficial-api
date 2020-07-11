cbac286babc8b624019488886535be5be9ba8ff1
Nabil Elqatib <nabilelqatib@gmail.com>
2020-07-11 23:10:32 +0200

    Update version in setup file

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="kooora",
-    version="0.1",
+    version="0.2",
     author="Nabil Elqatib",
     author_email="nabilelqatib@gmail.com",
     description="An unofficial Kooora API",

b622165f6e35eea4a57d79efa1a38fd59ab6bec9
Nabil Elqatib <nabil.elqatib@doctolib.com>
2023-10-23 11:51:46 +0200

    Restore setup.py

--- setup.py
+++ setup.py
@@ -16,6 +16,7 @@ setuptools.setup(
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
@@ -24,6 +25,6 @@ setuptools.setup(
         "Intended Audience :: Developers",
         "Topic :: Utilities",
     ],
-    python_requires='>=3.7',
+    python_requires='>=3.6',
 )
 
